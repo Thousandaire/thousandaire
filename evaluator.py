4016c7c5ab177c61aa0130c3bc456dbff1dfd30d
Hui Chih Shen <hcshen916@gmail.com>
2020-09-04 14:37:00 +0800

    Add evaluator.py This module provides several basic ways and optional self-defined method to evaluates alpha performance, which takes output from simulators and computes different measures.

---
+++ evaluator.py
@@ -0,0 +1,148 @@
+"""
+Evaluator calculates indicators for an alpha to evaluate alpha performance.
+"""
+
+from multiprocessing import Array, Process, Queue
+import ctypes
+import pickle
+import numpy as np
+
+DATES = 'dates'
+PNLS = 'pnls'
+POSITIONS_RAW = 'positions_raw'
+POSITIONS_NP = 'positions_np'
+indicators_all = {}
+indicators_default = []
+
+class Evaluator:
+    """
+    Evaluator to run alpha evaluation indicators.
+    """
+    def __init__(self, indicator_names=None):
+        """
+        indicators: indicators to calculate.
+            If indicators=None, default indicators will be calculated.
+        """
+        if indicator_names is None:
+            self.indicators = indicators_default
+        else:
+            self.set_indicators(indicator_names)
+
+    def get_indicators(self):
+        """
+        Return a list of current assigned indicator names.
+        """
+        return [indicator.__name__ for indicator in self.indicators]
+
+    def set_indicators(self, indicator_names):
+        """
+        Set to-run indicators to be the given indicators.
+        """
+        self.indicators = []
+        for indicator_name in indicator_names:
+            indicator = indicators_all.get(indicator_name)
+            if indicator is None:
+                raise AttributeError(
+                    'Indicator not found: %s' % indicator_name)
+            self.indicators.append(indicator)
+
+    def run(self, data):
+        """
+        Run all specified evaluation functions and return their results.
+
+        data: a Data with fields 'pnls' and 'positions'.
+                pnls: a list-like objects of floats.
+                positions: a list-like of dict-like objects.
+        """
+        dates = [item.date for item in data]
+        pnls = np.array([item.pnl for item in data])
+        positions_raw = [item.position_raw for item in data]
+        positions_np = np.array([item.position_np for item in data])
+        serialized = (
+            lambda var: Array(ctypes.c_char, pickle.dumps(var), lock=False))
+        packed = {
+            DATES: serialized(dates),
+            PNLS: serialized(pnls),
+            POSITIONS_RAW: serialized(positions_raw),
+            POSITIONS_NP: serialized(positions_np)
+        }
+        processes = []
+        results_queue = Queue()
+        for indicator in self.indicators:
+            process = Process(
+                target=evaluate,
+                args=(results_queue, indicator),
+                kwargs=packed)
+            process.start()
+            processes.append(process)
+        for process in processes:
+            process.join()
+        results = {}
+        while not results_queue.empty():
+            indicator_name, result = results_queue.get()
+            results[indicator_name] = result
+        return results
+
+def evaluate(results, indicator, **serialized):
+    """
+    Decode all shared variables into evalution function inputs, and return
+    their results by results (a multiprocess.Queue).
+    """
+    results.put((indicator.__name__, indicator(**serialized)))
+
+def get_all_indicators():
+    """
+    Return a list of all available built-in indicators.
+    """
+    return list(indicators_all.keys())
+
+def default(func):
+    """
+    Set func to be a default indicators.
+    """
+    indicators_default.append(func)
+    return func
+
+def inputs(*needed_fields):
+    """
+    Decorator for eval_functions to extract their inputs from encoded data.
+    Only fields in needed_fields will be decoded to improve the performance.
+
+    All and only functions decorated by this will be considered as indicators.
+    They will be registered into indicator_list for lookup.
+    """
+    def middle(func):
+        def final(**available_fileds):
+            return func(*(
+                pickle.loads(available_fileds[field])
+                for field in needed_fields))
+        final.__name__ = func.__name__
+        # register the function into indicators_all
+        indicators_all[final.__name__] = final
+        return final
+    return middle
+
+@default
+@inputs(PNLS)
+def returns(pnls):
+    """
+    Average annual returns
+    """
+    return np.mean(pnls) * 252
+
+@default
+@inputs(PNLS)
+def sharpe(pnls):
+    """
+    Sharpe ratio
+    """
+    return np.mean(pnls) / np.std(pnls)
+
+@default
+@inputs(POSITIONS_NP)
+def turnover(positions_np):
+    """
+    Turnover rate
+    """
+    daily_tvr = abs(positions_np[1:] - positions_np[:-1]).sum(axis=1) / 2
+    return {'Mean': daily_tvr.mean(), 'Std': daily_tvr.std()}
