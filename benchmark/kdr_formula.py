d1a6bd6a458eef9a04cea52d004b8bf52dd51c2a
Black <ioumu8@gmail.com>
2020-11-13 20:13:38 +0800

    Establish benchmark issue #20
    Build a benchmark to evaluate the efficiency of simulation process.
    1. Rename template to benchmark
    2. Produce KDR alpha.

--- benchmark/kdr_formula.py
+++ benchmark/kdr_formula.py
@@ -1,5 +1,5 @@
 """
-A template of alpha formula.
+KDR alpha formula.
 """
 
 from collections import deque
@@ -13,33 +13,33 @@ class AlphaFormula(BaseAlphaFormula):
     DO NOT overload the following methods, or simulation may fail in prod:
             __call__, get_last_success_date
     """
-    def __init__(self, _startdate, data, parameters):
+    def __init__(self, _startdate, dataset, parameters):
         """
         Set up the alpha before generating any portfolios.
         If the alpha needs to warm-up before returning its first portfolio.
         """
-        BaseAlphaFormula.__init__(self, _startdate, data, parameters)
+        BaseAlphaFormula.__init__(self, _startdate, dataset, parameters)
         self.k_days = parameters['k']
         self.mid_last_k_days = {
             instrument: deque(
                 (datum.buy + datum.sell) / 2
                 for datum in price_data[-self.k_days - 1: -1]
                 if all((datum.buy, datum.sell)))
-            for instrument, price_data in data['currency_price_tw'].items()
+            for instrument, price_data in dataset['currency_price_tw'].items()
         }
         self.sum_last_k_days = {
             instrument: sum(self.mid_last_k_days[instrument])
-            for instrument in data['currency_price_tw']
+            for instrument in dataset['currency_price_tw']
         }
 
-    def generate(self, _date, data):
+    def generate(self, _date, dataset):
         """
         Generate portfolio for the given date.
         The simulator guarantees to call this method in date order, and the
         first date will be "startdate" specified in AlphaSettings.
         """
         portfolio = Portfolio()
-        for instrument, price_data in data['currency_price_tw'].items():
+        for instrument, price_data in dataset['currency_price_tw'].items():
             if all((price_data[-1].buy, price_data[-1].sell)):
                 # maintain mid_last_k_days and sum_last_k_days for optimization.
                 new = (price_data[-1].buy + price_data[-1].sell) / 2
